cfa4f240545048d8e48b68caf33034e5e33564ee
George Silva <george.silva@delbank.com.br>
2025-02-15 10:49:42 -0300

    feat: configure database connection and repositories

---
+++ server/BankAccounts/BankAccounts.API/Endpoints/BankAccountsEndpoints.cs
@@ -0,0 +1,34 @@
+﻿using Microsoft.AspNetCore.Mvc;
+using System.Net;
+
+namespace BankAccounts.API.Endpoints;
+
+public static class BankAccountsEndpoints
+{
+    public static void AddBankAccountsEndpoints(this RouteGroupBuilder route)
+    {
+        var bankAccountRoute = route.MapGroup("bank-accounts");
+
+        bankAccountRoute.MapPost("/", async ([FromBody] CreateLocalityRequest request,
+                                         [FromServices] ICreateLocalityHandler handler,
+                                         CancellationToken cancellationToken) =>
+        {
+            var response = await handler.Handle(request, cancellationToken);
+
+            if (response.StatusCode == HttpStatusCode.BadRequest)
+                return Results.BadRequest(response);
+
+            if (response.StatusCode == HttpStatusCode.Conflict)
+                return Results.Conflict(response);
+
+            return Results.Created("localities/{id}", response);
+        })
+            .Produces(StatusCodes.Status201Created, typeof(CreatedLocalitySuccessfully))
+            .WithOpenApi(operation => new(operation)
+            {
+                Summary = "Registro de uma nova conta.",
+                Description = "Permite a inserção de uma nova conta bancária no sistema.",
+            })
+            .WithTags("Bank Accounts");
+    }
+}
---
+++ server/BankAccounts/BankAccounts.Infra/Repositories/UnitOfWork.cs
@@ -0,0 +1,36 @@
+﻿using BankAccounts.Domain.Interfaces.Repositories;
+using BankAccounts.Infra.Data.Context;
+using Microsoft.EntityFrameworkCore.Storage;
+
+namespace BankAccounts.Infra.Repositories;
+public class UnitOfWork : IUnitOfWork
+{
+    private IDbContextTransaction? _transaction;
+    private readonly BankAccountsContext _context;
+
+    public UnitOfWork(BankAccountsContext ibgeContext)
+    {
+        _context = ibgeContext;
+    }
+
+    public void BeginTransaction()
+    {
+        _transaction = _context.Database.BeginTransaction();
+    }
+
+    public async Task Commit(CancellationToken cancellationToken)
+    {
+        await _context.SaveChangesAsync(cancellationToken);
+        _transaction?.Commit();
+    }
+
+    public void Rollback()
+    {
+        _transaction?.Rollback();
+    }
+
+    public void Dispose()
+    {
+        _transaction?.Dispose();
+    }
+}
